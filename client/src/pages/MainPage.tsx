c1b84e26559b9dffb33a71e39bbe53a43da0f707
Juicestus <jus@gtsbr.org>
2025-03-23 09:58:56 -0500

    Acceptable understanding

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -178,7 +178,7 @@ export default () => {
 
   const getNextResponse = async (x: number, y: number, realCanvas: HTMLCanvasElement) => {
 
-    const query = "You are an educational assistant helping students with homework problems.\n"
+    let query = "You are an educational assistant helping students with homework problems.\n"
       + "The following image is the workspace of a student attempting to solve a homework problem.\n"
       + "Analyze the image to identify the problem and their work.\n"
       + "Please guide them through the *next step* they need to take.\n"
@@ -198,22 +198,30 @@ export default () => {
       + "Dont do any computation for the student. Only output *next step* that the student should perform. Be fairly brief and to the point.\n"
       ;
 
-
-
     let canvas = cloneCanvas(realCanvas);
-    
 
-    const ctx = canvas.getContext('2d');
-    if (ctx) {
-      for (const response of responses) {
-        console.log(response);
-        if (response.text === "") continue;
-        ctx.font = '14px Arial';
-        ctx.fillStyle = 'white';
-        // ctx.fillText(response.text, response.pos.x, response.pos.y + 30);
-        wrapText(ctx, response.text, response.pos.x, response.pos.y + 30, 800, 24);
+    let goodResponses = responses.filter(response => response.text !== ""); 
+
+    if (goodResponses.length > 0) {
+      query += "The student has already completed the following steps that you have given them:\n";
+      for (const response of goodResponses) {
+        query += "- " + response.text + "\n";
       }
+    } else {
+      query += "The student has not completed any steps yet.\n";
     }
+
+    // const ctx = canvas.getContext('2d');
+    // if (ctx) {
+    //   for (const response of responses) {
+    //     console.log(response);
+    //     if (response.text === "") continue;
+    //     ctx.font = '14px Arial';
+    //     ctx.fillStyle = 'white';
+    //     // ctx.fillText(response.text, response.pos.x, response.pos.y + 30);
+    //     wrapText(ctx, response.text, response.pos.x, response.pos.y + 30, 800, 24);
+    //   }
+    // }
 
     let croppedCanvas = cropCanvasToContentWithPadding(canvas, 20);
     if (croppedCanvas === null) {
